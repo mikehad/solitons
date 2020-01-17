62e14ae05738d66903d0caa9643a6c52c43fa697
mikehad <55763888+mikehad@users.noreply.github.com>
2020-01-17 14:23:40 +0000

    Add files via upload

--- main_numpy.py
+++ main_numpy.py
@@ -5,9 +5,9 @@ import time
 def main():
     system= verlet()
     system.velocity_verlet()
-    system.position_graph()
+    #system.position_graph()
     system.modes_graph()
-    system.energy_graph()
+    #system.energy_graph()
 
 
 
